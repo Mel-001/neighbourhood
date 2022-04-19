e6ea7511059eefd6412a41a8b94d6a7313d774cf
Melonie Langat <meloni.langat@student.moringaschool.com>
2022-04-19 23:10:59 +0300

    add app urls.py

---
+++ neighbourhood_app/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path 
+from . import views
+
+urlpatterns=[
+   path('',views.home, name='home'),
+   path('logout/', views.logoutUser, name='logout'),
+   path('profile/',views.profile,name = 'profile'),
+   path('comment/<int:id>/',views.comment,name='comment'),
+   path('newpost/',views.new_post,name='newpost'),
+   # path('newhood/',views.newhood,name='newhood'),
+   # path('newbiz/',views.newbiz,name='newbiz'),
+   path('post/<int:id>/',views.post,name='post'),
+   path('search/',views.search_results,name = 'search_results'),
+]
