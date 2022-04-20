a1d39476a3f544fd4be17c7c86dcde7b23e4b65b
Melonie Langat <meloni.langat@student.moringaschool.com>
2022-04-20 14:48:14 +0300

    app hood

--- hood/apps.py
+++ hood/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class NeighbourhoodAppConfig(AppConfig):
+class HoodConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'neighbourhood_app'
+    name = 'hood'
--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'neighbourhood_app',
+    'hood',
     'cloudinary',
     'tinymce',
     'bootstrap4'
